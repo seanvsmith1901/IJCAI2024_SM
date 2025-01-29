8783f27ebd9f2520f1f2f104a35fdbdda611a1d7
seanvsmith1901 <seanvsmith1901@gmail.com>
2025-01-29 15:02:24 -0700

    more adjustments
    while adjusting, I am switching back and forth between this branch and stable-single to try and gauge progress. I need to push these changes or it will stash them and we could potentially lose them.

--- Code/GeneSimulation_py/Server/simulator.py
+++ Code/GeneSimulation_py/Server/simulator.py
@@ -21,78 +21,96 @@ np.set_printoptions(precision=2, suppress=True)
 
 class Simulator():
     def __init__(self, num_human_players, num_players):
-        # what we need to do is take in the number of human players and generate agents from there.
-        self.sim = None # wait a minute, do I need this?
-        # now we gotta figure out how to create the players
-        # create the agents\
-        # fetch i am putthing this off lets figure out how to create the agents. 
-        # decide initial population (100 hard coded?)
-        # create the poverty line (not sure what THAT should look like)
-        # and i have no idea what forced random is supposed to be.
-        start_game(agents, initial_pops, poverty_line, forcedRandom)
+
+        start_game(num_human_players, num_players)
 
 
 
-def start_game(agents, initial_pops, poverty_line, forcedRandom):
-    global sim
-    players = [
-        *agents
-    ]
+    def start_game(self, num_human_players, num_players):
+        global sim
 
-    alpha_min, alpha_max = 0.20, 0.20
-    beta_min, beta_max = 0.5, 1.0
-    keep_min, keep_max = 0.95, 0.95
-    give_min, give_max = 1.30, 1.30
-    steal_min, steal_max = 1.6, 1.60
+        numAgents = num_players - num_human_players
+        configured_players = []
+        popSize = 3  # ??? I think? based on the command line arguemnts
+        player_idxs = list(np.arrange(0, numAgents))  # where numAgents is the number of actual agents, not players.
+        for _ in range(num_human_players):
+            configured_players.append(HumanAgent())
 
-    num_players = len(players)
-    base_pop = 100
-    tkns = num_players
+        for i in range(0, len(configured_players)):
+            player_idxs = np.append(player_idxs, popSize + i)
 
-    game_params = {
-        "num_players": num_players,
-        "alpha": alpha_min,  # np.random.uniform(alpha_min, alpha_max),
-        "beta": beta_min,  # np.random.uniform(beta_min, beta_max),
-        "keep": keep_min,  # np.random.uniform(keep_min, keep_max),
-        "give": give_min,  # np.random.uniform(give_min, give_max),
-        "steal": steal_min,  # np.random.uniform(steal_min, steal_max),
-        "poverty_line": poverty_line,
-        "base_popularity": np.array(initial_pops)
-        # "base_popularity": np.array([*[base_pop]*(num_players)])
-        # "base_popularity": np.array(random.sample(range(1, 200), num_players))
+        plyrs = []
+        for i in range(0, len(player_idxs)):
+            if player_idxs[i] >= popSize:
+                plyrs.append(configuredPlayers[player_idxs[i] - popSize])
+            else:
+                plyrs.append(theGenePools[player_idxs[i]])
+        players = np.array(plyrs)
+        agents = list(players)
 
-    }
-
-    for a in agents:  # sets the game params for all users.
-        a.setGameParams(game_params, forcedRandom)
-
-    sim = GameSimulator(
-        game_params)  # sets up our sim object - might need to make this global so we can grab it wherever we need it.
+        initial_pops = define_initial_pops(0, len(player_idxs))
+        poverty_line = 0
+        forcedRandom = False
 
 
-def execute_round(allocations):  # all of the player allocations will get passed in here
-    # print("\nRound: " + str(r))
-    # build allocations here.
+        players = [
+            *agents
+        ]
 
-    T = np.eye(num_players) * tkns
-    T_prev = sim.get_transaction()
+        alpha_min, alpha_max = 0.20, 0.20
+        beta_min, beta_max = 0.5, 1.0
+        keep_min, keep_max = 0.95, 0.95
+        give_min, give_max = 1.30, 1.30
+        steal_min, steal_max = 1.6, 1.60
 
-    # basically this is where all of the magic needs to happen. Oh, just make a while loop that checks for all player input. return T when finished.
+        num_players = len(players)
+        base_pop = 100
+        tkns = num_players
 
-    # T = sim.get_player_inputs(T)
+        game_params = {
+            "num_players": num_players,
+            "alpha": alpha_min,  # np.random.uniform(alpha_min, alpha_max),
+            "beta": beta_min,  # np.random.uniform(beta_min, beta_max),
+            "keep": keep_min,  # np.random.uniform(keep_min, keep_max),
+            "give": give_min,  # np.random.uniform(give_min, give_max),
+            "steal": steal_min,  # np.random.uniform(steal_min, steal_max),
+            "poverty_line": poverty_line,
+            "base_popularity": np.array(initial_pops)
+            # "base_popularity": np.array([*[base_pop]*(num_players)])
+            # "base_popularity": np.array(random.sample(range(1, 200), num_players))
 
-    # use this under the sim.get_player inputs to populate T. The problem! is that I have to distinguish between human and non human players.
-    for i, plyr in enumerate(players):  # DON"T RUN THIS UNITL YOU KNOW THAT YOU HAVE EVERYONE
-        T[i] = plyr.play_round(
-            i,  # player index
-            r,  # round
-            T_prev[:, i],  # received
-            sim.get_popularity(),  # popularity
-            sim.get_influence(),  # influence
-            sim.get_extra_data(i)  # could NOT tell you waht this is.
-        )
+        }
 
-    sim.play_round(T)
+        for a in agents:  # sets the game params for all users.
+            a.setGameParams(game_params, forcedRandom)
+
+        sim = GameSimulator(
+            game_params)  # sets up our sim object - might need to make this global so we can grab it wherever we need it.
+
+
+    def execute_round(self, allocations):  # all of the player allocations will get passed in here
+        # print("\nRound: " + str(r))
+        # build allocations here.
+
+        T = np.eye(num_players) * tkns
+        T_prev = sim.get_transaction()
+
+        # basically this is where all of the magic needs to happen. Oh, just make a while loop that checks for all player input. return T when finished.
+
+        # T = sim.get_player_inputs(T)
+
+        # use this under the sim.get_player inputs to populate T. The problem! is that I have to distinguish between human and non human players.
+        for i, plyr in enumerate(players):  # DON"T RUN THIS UNITL YOU KNOW THAT YOU HAVE EVERYONE
+            T[i] = plyr.play_round(
+                i,  # player index
+                r,  # round
+                T_prev[:, i],  # received
+                sim.get_popularity(),  # popularity
+                sim.get_influence(),  # influence
+                sim.get_extra_data(i)  # could NOT tell you waht this is.
+            )
+
+        sim.play_round(T)
 
 
 
