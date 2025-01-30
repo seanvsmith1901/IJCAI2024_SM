8fe8bcf08ffa355a94b399308b36208e4909a684
Garrett Webster <garrettreeds@gmail.com>
2025-01-29 20:07:44 -0700

    Changed JhgPanel to work off a grid, fixing some alignment problems and cutting down on code.

--- Code/GeneSimulation_py/Client/PyqtComponents/JhgPanel.py
+++ Code/GeneSimulation_py/Client/PyqtComponents/JhgPanel.py
@@ -1,12 +1,11 @@
 from functools import partial
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QWidget
+from PyQt6.QtWidgets import QHBoxLayout, QVBoxLayout, QLabel, QPushButton, QWidget, QGridLayout
 
 from .SubmitButton import SubmitButton
 
-
-class JhgPanel(QVBoxLayout):
+class JhgPanel(QGridLayout):
     def __init__(self, round_state, client_socket):
         super().__init__()
         # footer
@@ -21,44 +20,32 @@ class JhgPanel(QVBoxLayout):
         # Each column loops through the players and adds the respective element from the associated player class.
 
         # ID column - Displays the id + 1 (for human readability) of each player
-        player_id_column = QVBoxLayout()
-        player_id_column.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        player_id_column.addWidget(QLabel("Player"))
+        self.addWidget(QLabel("Player"), 0, 0)
         for i in range(0, 11):
-            player_id_column.addWidget(round_state.players[i].id_label)
+            self.addWidget(round_state.players[i].id_label, i + 1, 0)
 
         # Popularity column - Displays the popularity of the player at the start of the current round
-        popularity_column = QVBoxLayout()
-        popularity_column.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        popularity_column.addWidget(QLabel("Popularity"))
+        self.addWidget(QLabel("Popularity"), 0, 1)
         for i in range(0, 11):
-            popularity_column.addWidget(round_state.players[i].popularity_label)
+            self.addWidget(round_state.players[i].popularity_label, i + 1, 1)
 
         # Sent column - Displays the number of tokens that the client player sent to the associated player the last round
-        sent_column = QVBoxLayout()
-        sent_column.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        sent_column.addWidget(QLabel("Sent"))
+        self.addWidget(QLabel("Sent"), 0, 2)
         for i in range(0, 11):
-            sent_column.addWidget(round_state.players[i].sent_label)
+            self.addWidget(round_state.players[i].sent_label, i + 1, 2)
 
         # Received column - Displays the number of tokens that the client player received from the associated player the last round
-        received_column = QVBoxLayout()
-        received_column.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        received_column.addWidget(QLabel("Received"))
+        self.addWidget(QLabel("Received"), 0, 3)
         for i in range(0, 11):
-            received_column.addWidget(round_state.players[i].received_label)
+            self.addWidget(round_state.players[i].received_label, i + 1, 3)
 
         # Allocations column - Shows the number of tokens that the client player has allocated to the associated player,
         # as well as buttons to change that value
-        allocations_column = QVBoxLayout()
-        allocations_column.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        allocations_column.addWidget(QLabel("Allocations"))
+        self.addWidget(QLabel("Allocations"), 0, 4)
         for i in range(0, 11):
-            print(f"CID: {round_state.client_id} i: {i} type: {int(round_state.client_id)}")
             allocations_row = QHBoxLayout()
             if i == int(round_state.client_id):
                 allocations_row.addWidget(QLabel("You :)"))
-                print("Ya boi")
             else:
                 allocations_row.addWidget(round_state.players[i].minus_button, 2)
                 allocations_row.addWidget(round_state.players[i].allocation_box, .5)
@@ -69,17 +56,4 @@ class JhgPanel(QVBoxLayout):
                     partial(round_state.players[i].update_allocation_minus, round_state, self.token_label, i))
                 round_state.players[i].plus_button.update.connect(
                     partial(round_state.players[i].update_allocation_plus, round_state, self.token_label, i))
-
-            allocations_column.addLayout(allocations_row)
-
-        # Used to group the various columns and display them next to each other (as columns)
-        column_layout = QHBoxLayout()
-
-        column_layout.addLayout(player_id_column)
-        column_layout.addLayout(popularity_column)
-        column_layout.addLayout(sent_column)
-        column_layout.addLayout(received_column)
-        column_layout.addLayout(allocations_column)
-
-        self.addLayout(column_layout)
-        self.addLayout(footer)
+            self.addLayout(allocations_row, i + 1, 4)
