6ff9e0cf1ef97200aaeca81cef6cef546fb8acf8
Garrett Webster <garrettreeds@gmail.com>
2025-03-19 16:52:11 -0600

    Reworked the client so that the JHG and SC are side by side.

--- Code/GeneSimulation_py/Client/PyqtComponents/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is just to allow for relative path imports. Don't delete, but do go yell at the python devs for a bad
-#  (A.K.A I probably just don't fully understand it) import system
-import os
-import sys
-
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
