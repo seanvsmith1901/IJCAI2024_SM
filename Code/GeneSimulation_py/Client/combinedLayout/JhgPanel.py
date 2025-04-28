b86c8c8711487174af86f8cd3981a620e89cf88c
Garrett Webster <garrettreeds@gmail.com>
2025-04-28 10:26:13 -0600

    Add tooltips to the popularity graph so that you can tell which player is which on the graph more easily.

--- Code/GeneSimulation_py/Client/combinedLayout/JhgPanel.py
+++ Code/GeneSimulation_py/Client/combinedLayout/JhgPanel.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget, QTabWidget, QHBoxLayout
+from PyQt6.QtWidgets import QWidget, QHBoxLayout
 from .JhgVotingPanel import JhgVotingPanel
 
 
@@ -13,5 +13,4 @@ class JhgPanel(QHBoxLayout):
         jhg_voting_panel.setLayout(voting_layout)  # Set the layout to the QWidget
 
         JHG_panel_layout.addWidget(jhg_voting_panel)
-        # JHG_panel_layout.addWidget(plots_panel)
         self.addLayout(JHG_panel_layout)
--- Code/GeneSimulation_py/Client/combinedLayout/ui_functions/JHG_functions.py
+++ Code/GeneSimulation_py/Client/combinedLayout/ui_functions/JHG_functions.py
@@ -1,12 +1,40 @@
 import pyqtgraph as pg
+from PyQt6.QtCore import QPoint
+from PyQt6.QtWidgets import QToolTip
+from PyQt6.QtGui import QCursor
 
 from .jhg_network_graph import update_jhg_network_graph
 from ..colors import COLORS
 
 
+# Custom ScatterPlotItem that supports tooltips
+class HoverScatter(pg.ScatterPlotItem):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setAcceptHoverEvents(True)
+
+
+    def hoverEvent(self, event):
+        if event.isExit():
+            QToolTip.showText(QCursor.pos(), "")
+            return
+        points = self.pointsAt(event.pos())
+        if points:
+            point = points[0]
+            cursor_pos = QCursor.pos()
+
+            # Create an offset to show the tooltip to the right of the cursor
+            offset = QPoint(10, -30)  # Adjust the (10, 0) values as needed for spacing
+
+            # Set the tooltip position
+            tooltip_position = cursor_pos + offset
+
+            # Show the tooltip (default system look)
+            QToolTip.showText(tooltip_position, str(point.data()))
+
+
 def update_jhg_ui_elements(main_window):
     for i in range(main_window.round_state.num_players):
-        # If players[i] is the client, show tokens kept. Else, show the received and sent tokens for that player
         if i == int(main_window.round_state.client_id):
             main_window.round_state.players[i].kept_number_label.setText(str(int(main_window.round_state.received[i])))
         else:
@@ -18,25 +46,45 @@ def update_jhg_ui_elements(main_window):
             str(round(main_window.round_state.message["POPULARITY"][i])))
         main_window.round_state.players[i].popularity_over_time.append(main_window.round_state.message["POPULARITY"][i])
         main_window.round_state.players[i].allocation_box.setText("0")
-        # pen = pg.mkPen(COLORS[i])
+
         update_jhg_popularity_graph(main_window.round_state, main_window.jhg_popularity_graph)
-        # main_window.jhg_popularity_graph.plot(main_window.round_state.players[i].popularity_over_time, pen=pen)
 
 
 def update_jhg_popularity_graph(round_state, jhg_popularity_graph):
+    jhg_popularity_graph.clear()
     max_popularity = 0
-    for i in range(len(round_state.players)):
-        pen = pg.mkPen(COLORS[i])
-        jhg_popularity_graph.plot(round_state.players[i].popularity_over_time, pen=pen)
-        if max(round_state.players[i].popularity_over_time) > max_popularity:
-            max_popularity = max(round_state.players[i].popularity_over_time)
+
+    for i, player in enumerate(round_state.players):
+        color = COLORS[i]
+        pen = pg.mkPen(color, width=2)
+
+        x = list(range(len(player.popularity_over_time)))
+        y = player.popularity_over_time
+
+        # Main line
+        jhg_popularity_graph.plot(x, y, pen=pen)
+
+        # Tooltip spots
+        spots = [
+            {'pos': (x[j], y[j]), 'data': f"{player.id + 1}", 'brush': pg.mkBrush(color), 'size': 10, 'pen': None}
+            for j in range(len(x))
+        ]
+
+        scatter = HoverScatter(spots=spots)
+        jhg_popularity_graph.addItem(scatter)
+
+        max_popularity = max(max_popularity, max(y))
 
     view_box = jhg_popularity_graph.getViewBox()
-    view_box.setLimits(xMin=0, xMax =round_state.jhg_round_num + 1, yMin=0, yMax=max_popularity + 10)
+    view_box.setLimits(
+        xMin=0,
+        xMax=round_state.jhg_round_num + 1,
+        yMin=0,
+        yMax=max_popularity + 10,
+    )
 
     jhg_popularity_graph.setXRange(0, round_state.jhg_round_num + 1, padding=0)
     jhg_popularity_graph.setYRange(0, max_popularity + 10, padding=0)
-
 
 
 def jhg_over(main_window):
@@ -45,7 +93,6 @@ def jhg_over(main_window):
 
     update_jhg_network_graph(main_window)
 
-    # Enable the SC buttons
     for button in main_window.SC_voting_grid.buttons:
         if button.objectName() != "clear_button":
             button.setEnabled(True)
